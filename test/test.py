86d1482b7b516b6220a5ecb20cc4447bed87b285
s.soler <s.soler@alumno.um.edu.ar>
2024-08-13 11:42:01 -0300

    test

--- test/test.py
+++ test/test.py
@@ -1,9 +1,10 @@
 import unittest
 
-from game.board import Board
-from game.chess import Chess
 
 
 class TestChess(unittest.TestCase):
-    def test_move_chess(self):
-        ...
+    def test_main(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
